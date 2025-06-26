7ed15b6b7d074678d3e28c22586c8d6fbcd8de95
aleferna12 <64274141+aleferna12@users.noreply.github.com>
2025-06-26 01:29:31 +0100

    impl std::error::Error trait for custom Error type

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,8 @@ impl Error {
     }
 }
 
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         write!(
